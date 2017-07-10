097375d5e085721aa637ee959e09c690d0d1125f
Quinntyne Brown <quinntynebrown@gmail.com>
2017-07-10 18:58:22 -0400

    Done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+using static System.Console;
+using static System.Convert;
+
+namespace SymmetricKeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider();
+            tripleDESCryptoServiceProvider.GenerateKey();
+            WriteLine(ToBase64String(tripleDESCryptoServiceProvider.Key));
+            WriteLine("Done!");
+            ReadLine();
+        }
+    }
+}
